5f91308047cccd5be058c56e4236681f8eb7af23
Oleksii Kravchenko <djdesidos@gmail.com>
2023-04-20 22:25:09 +0300

    fix-product-process

--- resources/js/js/product.js
+++ resources/js/js/product.js
@@ -7,19 +7,36 @@ items.forEach((item) => {
         el.classList.remove("active");
       }
     });
+
     event.currentTarget.classList.toggle("active");
 
     items.forEach((item) => {
       Array.from(item.children).forEach((child) => {
-        child.classList?.remove("active");
+        Array.from(child.children).forEach((child) => {
+          child.classList?.remove("active");
+        });
       });
     });
 
     if (event.currentTarget.classList.contains("active")) {
-      const [_, productsDescription, productsIcon] = event.currentTarget.children;
+      const [_, productsDescription, productsIcon] =
+        event.currentTarget.children[0].children;
       productsDescription.classList.add("active");
       productsIcon.classList.add("active");
     }
   });
 });
 
+const body = document.querySelector("body"),
+  observ = new MutationObserver(() => {
+    const itemsActive = document.querySelector(".products__item.active");
+    if (itemsActive) {
+      itemsActive.style.zIndex = 2;
+      itemsActive.addEventListener("click", (e) => {
+        setTimeout(() => {
+          itemsActive.style.zIndex = 1;
+        }, 1000);
+      });
+    }
+  });
+observ.observe(body, { childList: true, subtree: true, attributes: true });
