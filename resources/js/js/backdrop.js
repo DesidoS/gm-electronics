b13d0ea70ddee6f1ef046dcbc65cb6e5218588de
Oleksii Kravchenko <djdesidos@gmail.com>
2023-05-02 18:42:15 +0300

    .

--- resources/js/js/backdrop.js
+++ resources/js/js/backdrop.js
@@ -1,48 +1,32 @@
-if (!window.matchMedia("(min-width: 1025px)").matches) {
-  const header = {
-    menuBtn: document.querySelector(".header__menu-button"),
-    element: document.querySelector(".header"),
-    navList: document.querySelector(".header__nav-list"),
-    navItem: document.querySelectorAll(".header__nav-item"),
-    navMenu: document.querySelector(".header__menu"),
-    contact: document.querySelector(".header__contact"),
-    menuClose: document.querySelector(".header__menu-close"),
-    langBtn: document.querySelector(".header__lang-button"),
-    logo: document.querySelector(".header__logo-svg"),
-    logoFooter: document.querySelector(".header__logo-svg--footer"),
-  };
+const header = {
+  menuBtn: document.querySelector(".header__menu-button"),
+  element: document.querySelector(".header"),
+  navItem: document.querySelectorAll(".header__nav-item"),
+};
 
-  function openBackdrop() {
-    window.addEventListener("resize", watchScreenHeight);
+function openBackdrop() {
+  window.addEventListener("resize", watchScreenHeight);
 
-    watchScreenHeight();
+  watchScreenHeight();
 
-    if (document.body.style.overflowY === "hidden") {
-      document.body.style.overflowY = "auto";
-    } else {
-      document.body.style.overflowY = "hidden";
-    }
-
-    header.langBtn.classList.toggle("is-open");
-    header.element.classList.toggle("is-open");
-    header.logo.classList.toggle("is-open");
-    header.logoFooter.classList.toggle("is-open");
-    header.navList.classList.toggle("is-open");
-    header.contact.classList.toggle("is-open");
-    header.navMenu.classList.toggle("is-open");
-    header.menuClose.classList.toggle("is-open");
-
-    if (!header.element.classList.contains("is-open")) {
-      window.removeEventListener("resize", watchScreenHeight);
-      header.element.classList.remove("beckdrop-scroll");
-    }
+  if (document.body.style.overflowY === "hidden") {
+    document.body.style.overflowY = "auto";
+  } else {
+    document.body.style.overflowY = "hidden";
   }
 
-  function watchScreenHeight() {
-    if (window.innerHeight > 632) return;
-    header.element.classList.add("beckdrop-scroll");
-  }
+  header.element.classList.toggle("is-open");
 
-  header.menuBtn.addEventListener("click", openBackdrop);
-  header.navItem.forEach((el) => el.addEventListener("click", openBackdrop));
+  if (!header.element.classList.contains("is-open")) {
+    window.removeEventListener("resize", watchScreenHeight);
+    header.element.classList.remove("beckdrop-scroll");
+  }
 }
+
+function watchScreenHeight() {
+  if (window.innerHeight > 632) return;
+  header.element.classList.add("beckdrop-scroll");
+}
+
+header.menuBtn.addEventListener("click", openBackdrop);
+header.navItem.forEach((el) => el.addEventListener("click", openBackdrop));
