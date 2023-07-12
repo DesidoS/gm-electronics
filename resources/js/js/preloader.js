d7bbe0c695bdf79f32bc828ee997a5f3fadfc36e
Oleksii <djdesidos@gmail.com>
2023-07-12 16:39:39 +0300

    change-preloader

--- resources/js/js/preloader.js
+++ resources/js/js/preloader.js
@@ -1,10 +1,33 @@
 const preloaderRef = document.querySelector(".preloader");
-let delay = window.innerWidth > 1025 ? 500 : 5500;
-document.body.style.overflow = "hidden";
+let delay = window.innerWidth > 1024 ? 500 : 2800;
+
+if (window.innerWidth <= 1024) {
+    setTimeout(() => {
+        document.querySelector(".preloader").style.transform =
+            "translateY(-500%)";
+        document.body.style.overflowY = "auto";
+    }, delay);
+}
+
+if (window.innerWidth > 1024) {
+    //схлопывание
+    // setTimeout(() => {
+    //     document.querySelector(".preloader").style.transform = "scaleX(0)";
+    //     document.querySelector(".preloader").style.opacity = "0";
+    //     document.querySelector(".preloader").style.transition = "all 0.5s ease";
+
+    //     document.body.style.overflowY = "auto";
+    // }, delay);
+    //схлопывание
+
+    setTimeout(() => {
+        document.querySelector(".preloader").style.transform = "scale(3)";
+        document.querySelector(".preloader").style.opacity = "0";
+        document.querySelector(".preloader").style.transition = "all 1s ease";
+
+        document.body.style.overflowY = "auto";
+    }, delay);
+}
 setTimeout(() => {
-  document.querySelector(".preloader").style.transform = "translateY(-500%)";
-  document.body.style.overflow = "";
-}, delay);
-setTimeout(() => {
-  document.querySelector(".preloader").classList.add("active");
+    document.querySelector(".preloader").classList.add("active");
 }, delay + 500);
